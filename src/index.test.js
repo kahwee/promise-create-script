2a8fb30120765a9818456cfa22e68daff1a2b157
KahWee Teng <t@kw.sg>
2017-08-14 18:15:21 -0700

    fix test cases

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-const createScript = require('./index')
+import createScript from './index'
 
 it('should be able to create script with valid url', () => {
   const uri = 'http://code.jquery.com/jquery-3.2.1.slim.min.js'
